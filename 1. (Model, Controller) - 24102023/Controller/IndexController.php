04cafe3a090f05c2dc1707c021bd34bb4893216b
Günbay Sali <136761286+Guenbay@users.noreply.github.com>
2023-10-25 14:05:27 +0200

    Create IndexController.php

---
+++ 1. (Model, Controller) - 24102023/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MyApp\Controller;
+
+class IndexController
+{    
+    public function indexAction()
+    {
+        echo "das ist ukjghlhgkjlnsere Index Action im Index Controller";
+    }
+}
+
+?>
