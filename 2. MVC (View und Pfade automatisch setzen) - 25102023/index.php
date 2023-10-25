d8ba486a9f0d0d9bdd7ecc83bdacf7c1e8b57903
Günbay Sali <136761286+Guenbay@users.noreply.github.com>
2023-10-25 14:07:09 +0200

    Create index.php

---
+++ 2. MVC (View und Pfade automatisch setzen) - 25102023/index.php
@@ -0,0 +1,60 @@
+<?php
+
+//php.net autoloader
+//include ("datei.php");
+
+spl_autoload_register(function ($class) 
+{
+    $prefix = 'MyApp\\';
+
+    $base_dir = __DIR__ .'/';
+
+    $len = strlen($prefix);
+
+    if (strncmp($prefix, $class, $len) !== 0) 
+    {
+        return;
+    }
+
+    $relative_class = substr($class, $len);
+    
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    if (file_exists($file)) 
+    {
+        require $file;
+    }
+});
+
+//$classname = "MyApp\\Controller\TestDatei";
+//$test = new $classname;
+
+
+//index.php?controller=index&action=index
+$controllerName = ucfirst(isset($_GET['controller']) ? $_GET['controller'] : "index");
+
+$actionName = ucfirst(isset($_GET['action']) ? $_GET['action'] : "index");
+
+$controllerClassName = "\\MyApp\\Controller\\".$controllerName."Controller";
+
+$actionMethodName = $actionName."Action";
+
+try {
+
+    $controller = new $controllerClassName();
+
+    if(!class_exists($controllerClassName))
+    {
+        throw new \Exception("".$controllerClassName."");
+    }
+
+
+    $controller->$actionMethodName();
+
+    //$path, $controller, $action
+    $view = new MyApp\Library\View(__DIR__.DIRECTORY_SEPARATOR."views", $_GET['controller'], $_GET['action']);
+    $view->render();
+    
+} catch (\Exception $message) {
+    echo $message->getMessage();
+}
